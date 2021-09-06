d618d7d8bdc5294f83dfb0132bf136c8b5e381de
ugurnur <ugur-n@yandex.com>
2021-09-06 12:54:39 -0400

    page20

---
+++ js/questions.js
@@ -0,0 +1,21 @@
+var questions19 = [
+    `25. Smoke Testing`,
+    `26. Which part of regression test should be automated?`,
+    `27. How do you ensure that your regression tests are effective?`,
+    `28. A number of critical bugs are fixed in software. All the bugs are in one module, related to reports. The test manager decides to do regression testing only on the report’s module.`,
+    `29. Which technique can be used to achieve input and output coverage?`,
+    `30. How do you run your regression? How often, how many VMs, how many days, how many tests?`,
+    `31. What is black box testing? What are the different black box testing techniques?`,
+    `32. What is Equivalence partitioning testing?`
+]
+
+var questions20 = [
+    `33. What is Boundary value testing?`,
+    `34. Why does the boundary value analysis provide good test cases?`,
+    `35. Why we use decision tables?`,
+    `36. What is white box testing and list the types of white box testing?`,
+    `37. In white box testing what do you verify?`,
+    `38. What is Gray Box Testing?`,
+    `39. What is the difference between static and dynamic testing?`,
+    `40. What is maintenance testing?`
+]
